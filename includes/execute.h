dd91295bfa79e938ca97ce563a097e779bfce041
CommanderSpinner <klaus.kutulu@gmail.com>
2024-12-25 22:12:08 +0100

    indentation

--- includes/execute.h
+++ includes/execute.h
@@ -4,16 +4,16 @@
 #include <stdbool.h>
 #include <gtk/gtk.h>
 
-struct execute{
-    char *script;
-    char **lines_of_script;
-    int count_lines;
+struct execute {
+	char *script;
+	char **lines_of_script;
+	int count_lines;
 };
 
 void create_script(struct execute *exec, unsigned int size);
 bool execute_script(struct execute *exec);
-void create_exec(struct execute** exec);
-void split_script_lines(struct execute* exec);
-void cleanup_script(struct execute* exec);
+void create_exec(struct execute **exec);
+void split_script_lines(struct execute *exec);
+void cleanup_script(struct execute *exec);
 
 #endif
